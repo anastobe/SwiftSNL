1a4cb0953940411e60f94f6df0a59568cd961ca8
anastobe <anastobe968@gmail.com>
2025-04-10 17:07:13 +0500

    Initial Commit

---
+++ SwiftSNL/SwiftSNLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSNLApp.swift
+//  SwiftSNL
+//
+//  Created by Peter on 10/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSNLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftSNLTests/SwiftSNLTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSNLTests.swift
+//  SwiftSNLTests
+//
+//  Created by Peter on 10/04/2025.
+//
+
+import Testing
+@testable import SwiftSNL
+
+struct SwiftSNLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
