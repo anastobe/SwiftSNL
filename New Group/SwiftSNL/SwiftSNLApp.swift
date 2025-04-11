79cef946eab97325cbbfe72e8d96a3beba906ba1
anastobe <anastobe968@gmail.com>
2025-04-11 17:15:06 +0500

    work

--- New Group/SwiftSNL/SwiftSNLApp.swift
+++ New Group/SwiftSNL/SwiftSNLApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftSNLApp: App {
     var body: some Scene {
         WindowGroup {
-            WelcomeView()
+            ContentView()
         }
     }
 }
---
+++ SwiftSNL/View/Login/WelcomeView.swift
@@ -0,0 +1,27 @@
+//
+//  WelcomeView.swift
+//  SwiftSNL
+//
+//  Created by Peter on 11/04/2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        ZStack {
+            Image("welcom_bg")
+                .resizable()
+                .scaledToFill()
+        
+            
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
